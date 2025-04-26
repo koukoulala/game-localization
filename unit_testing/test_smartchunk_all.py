b49331aa972ad25c0111bd330392e5270f70aa71
Abdallah Ali <abdallah.ali.abdallah.elmasry@gmail.com>
2025-04-26 18:29:55 +0300

    update unit testing location

--- unit_testing/test_smartchunk_all.py
+++ unit_testing/test_smartchunk_all.py
@@ -2,9 +2,9 @@ import pytest
 import sys
 import os
 
-# Add the current directory to the path so we can import the module
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from smartchunk import SmartChunker
+# Add the parent directory to the path so we can import the module
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from src.smartchunk import SmartChunker
 
 # --- Fixtures ---
 @pytest.fixture(scope="module")
